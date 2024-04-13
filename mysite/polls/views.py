ea08441e9792a2d2434170c49eda2ebd64174d4b
Dalma Muñoz <dalma.mdf@gmail.com>
2024-04-13 16:16:52 -0300

    [Parte 3.5] Agrega una vista de un error 404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -33,3 +34,11 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
+
+#metodo detail de generar un error 404
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
